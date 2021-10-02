bf20956e3f2b288a0d4f5aaacdef57c15bd8fe00
zfk <zfk@qq.com>
2021-10-02 13:23:17 +0800

    hot commit2

--- src/main/java/pojo/People.java
+++ src/main/java/pojo/People.java
@@ -23,6 +23,8 @@ public class People {
         System.out.println("步骤二");
         System.out.println("步骤三");
         System.out.println("hot-fix 操作");
+        System.out.println("hot-fix 操作2");
+
     }
 
 
